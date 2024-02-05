ddf22aef62ba73fd134685961a84594d1430a063
giangdx <giangdx@unicloud.com.vn>
2024-02-05 17:16:33 +0700

    add first unique character in a string (wip)

---
+++ dart/hashmap/first_unique_character_in_a_string.dart
@@ -0,0 +1,29 @@
+class Solution {
+  int firstUniqueChar(String s) {
+    int uniqueCharCount = -1;
+    if (s.isEmpty) {
+      return uniqueCharCount;
+    }
+    List<String> splittedStr = s.split('');
+    Map<String, int> hashMap = {};
+    for (var i = 0; i < splittedStr.length; i++) {
+      hashMap[splittedStr[i]] = (hashMap[splittedStr[i]] ?? 0) + 1;
+    }
+    final entries = hashMap.entries.toList();
+    print(entries);
+    for (var i = 0; i < entries.length; i++) {
+      if (entries[i] == 1) {
+        uniqueCharCount = i;
+        print(uniqueCharCount);
+        return uniqueCharCount;
+      }
+    }
+    return uniqueCharCount;
+  }
+}
+
+void main(List<String> args) {
+  var s = 'dddccdbba'; // failed test case
+  final obj = Solution();
+  var result = obj.firstUniqueChar(s);
+}
