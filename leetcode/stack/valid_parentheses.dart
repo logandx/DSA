28c6158575705cb1a51586ad8700e7faf35566f1
dxgiang <dinhgiang2k@gmail.com>
2024-03-03 22:44:44 +0700

    add remove stars from a string

---
+++ leetcode/stack/valid_parentheses.dart
@@ -0,0 +1,48 @@
+// /* 20. Valid Parentheses
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+// determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'. 
+// */
+
+// class Solution {
+//   bool isValid(String s) {
+//     final map = <String, String>{
+//       "[": "]",
+//       "{": "}",
+//       "(": ")",
+//     };
+//     final list = <String>[];
+//     for (var i = 0; i < s.split('').length; i++) {
+//     }
+//     return list.isEmpty;
+//   }
+// }
