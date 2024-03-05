9c39f35b9fae46cd917101dba09e19a1b639e195
giangdx <giangdx@unicloud.com.vn>
2024-03-05 17:24:29 +0700

    add product of array except self with more standard way of prefixsum

--- leetcode/prefix_sum/go/product_of_array_except_self.go
+++ leetcode/prefix_sum/go/product_of_array_except_self.go
@@ -14,3 +14,19 @@ func productExceptSelf(nums []int) []int {
 	}
 	return result
 }
+
+// / This seems a standard way of prefix sum approach
+func productExceptSelf2(nums []int) []int {
+	result := make([]int, len(nums))
+	result[0] = 1
+	result[len(nums)-1] = 1
+	postfix := 1
+	for i := 1; i < len(nums); i++ {
+		result[i] = result[i-1] * nums[i-1]
+	}
+	for i := len(nums) - 2; i >= 0; i-- {
+		postfix *= nums[i+1]
+		result[i] *= postfix
+	}
+	return result
+}
