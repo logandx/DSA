80cf7454148d1dc59c907d12d9f27ae60f735ae4
giangdx <giangdx@unicloud.com.vn>
2024-03-05 15:28:29 +0700

    add notes for prefix/postfix sum

--- dart/prefix_sum/test.dart
+++ dart/prefix_sum/test.dart
@@ -1,7 +1,22 @@
+/* 
+For ones, who did not understand how prefix-postfix works, lets change
+ 1, 2, 3, 4 positions to symbols like a, b, c, d, so multiplying will be:
+prefix:
+->
+|       a       |   a*b   | a*b*c | a*b*c*d |
+postfix:
+<-
+| a*b*c*d | b*c*d |   c*d   |      d        |
+
+The result is a multiply without the symbol in own position (the left value from
+ prefix and the right one from postfix):
+|    b*c*d  | a*c*d | a*b*d |   a*b*c   |
+ */
+
 class PrefixSum {
   /// Time Complexity: O(N)
   /// Auxiliary Space: O(N)
-  List<int> calculate(List<int> nums) {
+  List<int> calculatePrefix(List<int> nums) {
     final prefixSum = List<int>.filled(nums.length, 0);
     prefixSum[0] = nums[0];
     for (var i = 1; i < nums.length; i++) {
@@ -13,8 +28,19 @@ class PrefixSum {
   }
 }
 
+class PostfixSum {
+  List<int> calculatePostfix(List<int> nums) {
+    final postfixSum = List<int>.filled(nums.length, 0);
+    postfixSum.last = nums.last;
+    for (var i = nums.length - 2; i >= 0; i--) {
+      postfixSum[i] = postfixSum[i + 1] + nums[i];
+    }
+    return postfixSum;
+  }
+}
+
 void main(List<String> args) {
   final object = PrefixSum();
-  final nums = [10, 4, 16, 20];
-  print(object.calculate(nums));
+  final nums = [1, 2, 3, 4];
+  print(object.calculatePrefix(nums));
 }
--- leetcode/prefix_sum/product_of_array_except_self.dart
+++ leetcode/prefix_sum/product_of_array_except_self.dart
@@ -1,4 +1,32 @@
 /**
+ * Given an integer array nums, return an array answer such that answer[i] is 
+ * equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? 
+(The output array does not count as extra space for space complexity analysis.)
+
+
+ * 
+ * 
  * Product of an array except itself demonstration
  *
  * This example using multiply arithmetic
@@ -22,6 +50,17 @@
  *
  */
 
+/// How can I get the logic?
+/// You can get the idea behind it by reverse engineering
+/// What does it mean by the product of everything except itself?
+/// => So the product of all elements but not the element itself
+/// 
+/// => Why do we access those elements?
+/// 
+/// => Two kinds of elements that make up the product => One before this Nth
+/// element, ones after this element
+/// 
+/// => Keep track of them and multiply
 class Solution {
   List<int> productExceptSelf(List<int> nums) {
     List<int> res = List<int>.filled(nums.length, 1);
@@ -36,6 +75,19 @@ class Solution {
       postfix *= nums[i];
     }
     return res;
+  }
+}
+
+class TheSolutionThatWontWorksOnON {
+  List<int> productExceptSelf(List<int> nums) {
+    final result = <int>[];
+    for (var i = 0; i < nums.length; i++) {
+      final newList = List.from(nums);
+      newList.removeAt(i);
+      var calculatedValue = newList.reduce((value, element) => value * element);
+      result.add(calculatedValue);
+    }
+    return result;
   }
 }
 
