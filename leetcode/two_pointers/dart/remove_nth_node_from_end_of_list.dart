66143325f0694e5277686c9addfd46502c962a56
giangdx <giangdx@unicloud.com.vn>
2024-03-20 09:34:25 +0700

    add comment

--- leetcode/two_pointers/dart/remove_nth_node_from_end_of_list.dart
+++ leetcode/two_pointers/dart/remove_nth_node_from_end_of_list.dart
@@ -9,15 +9,24 @@ class Solution {
     ListNode dummy = ListNode(0, head);
     ListNode? left = dummy;
     ListNode? right = head;
+
+    /// Shifting right by n
     while (n > 0 && right != null) {
       right = right.next;
       n--;
     }
+
+    /// Shifting left and right
     while (right != null) {
       right = right.next;
       left = left?.next;
     }
-    left?.next = left.next?.next;
+
+    // Assign left's next pointer equals to its next pointer
+    // left
+    // 1 -> 2 -> 3
+    // left is in 1 so its next pointer is 3
+    left?.next = left.next?.next; 
     return dummy.next;
   }
 }
